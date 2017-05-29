3334d10e282bd5aa9a56809d39db2e236ac8514f
phanmanhcuong <you@example.com>
2017-05-29 10:43:01 +0700

    done

---
+++ system.c
@@ -0,0 +1,205 @@
+#include <stdio.h> 
+#include <stdlib.h>
+#include <string.h>   
+#include <sys/socket.h>   
+#include <arpa/inet.h> 
+#include <time.h>
+#include <sys/types.h> 
+#include <sys/ipc.h> 
+#include <sys/shm.h> 
+
+#define SHIMSIZE 10
+
+int main(){
+	int listenfd = 0, connfd = 0, pid, recvm, c;
+    struct sockaddr_in serv_addr, cli_addr; 
+    char *clientMessage, *machineID, *brandName, *stock, *date, *month, *year, *hour, *minute, *second;
+  	char *shmMessage;
+    FILE *historyFile, *inventoryFile;
+    key_t key = 1234;
+    int shmidWrite, shmidRead, *head, *begin;
+    char *invent, *inventMachineID, *inventBrandName, *inventStock, *inventAmount;
+    int confirm, stockNumber, msgFlag = 0;
+    char character, success[50], error[50];
+
+    listenfd = socket(AF_INET, SOCK_STREAM, 0);
+    if(listenfd == -1){
+    	printf("Create socket failed !\n");
+    	return 0;
+    }
+    memset(&serv_addr, '0', sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(8888);
+    if(bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0){
+    	printf("Bind failed !\n");
+    	return 0;
+    }
+
+    listen(listenfd, 3);
+    c = sizeof(struct sockaddr_in);
+    for(;;){
+    	connfd = accept(listenfd, (struct sockaddr*)&cli_addr, (socklen_t*)&c);
+    	pid = fork();
+    	if(pid < 0){
+    		close(connfd);
+    	}
+    	else if (pid > 0){
+    		close(connfd);
+    	}
+    	else{
+    		if(connfd < 0){
+    			printf("Accept failed !\n");
+    			return 1;
+    		}
+
+    		//read from socket
+    		clientMessage = (char *)malloc(100 *(sizeof(char)));
+    		invent = (char *)malloc(100 *(sizeof(char)));
+    		for(;;){
+    			recvm = recv(connfd, clientMessage, 100, 0);
+    			machineID = strtok(clientMessage, ",");
+    			brandName = strtok(NULL, ",");
+    			stock = strtok(NULL, ",");
+    			date = strtok(NULL, ",");
+    			month = strtok(NULL, ",");
+    			year = strtok(NULL, ",");
+    			hour = strtok(NULL, ",");
+    			minute = strtok(NULL, ",");
+    			second = strtok(NULL, ",");
+    			
+    			printf("Purchased Commodity: %s\n",brandName);
+
+    			//write to history file
+    			if((historyFile = fopen("history.txt", "r+")) == NULL){
+    				printf("History file doesn't exist !\n");
+    			}
+
+    			fseek(historyFile, 0, SEEK_END);
+    			fprintf(historyFile, "%s\t%s\t%s/%s/%s %s:%s:%s\n", machineID, brandName, date, month, year, hour, minute, second);
+    			fclose(historyFile);
+
+    			//write to inventory file
+    			if((inventoryFile = fopen("inventory.txt", "r+")) == NULL){
+    				printf("Inventory file doesn't exist !\n");
+    			}
+    			while((fgets(invent, 100, inventoryFile)) != NULL){
+    				inventMachineID = strtok(invent, ",");
+    				inventBrandName = strtok(NULL, ",");
+    				inventStock = strtok(NULL, ",");
+
+    				//compare machineID and brandName
+    				if((strcmp(inventMachineID, machineID)) == 0 && (strcmp(inventBrandName, brandName)) == 0){
+    					fseek(inventoryFile, -4, SEEK_CUR);
+    					while((character = fgetc(inventoryFile)) != '\n'){
+    						if(isdigit((int)character)){
+    							fseek(inventoryFile, -1, SEEK_CUR);
+    							fprintf(inventoryFile, "%s\n", stock);
+    							fseek(inventoryFile, 1, SEEK_CUR);
+    							//send success message
+    							sprintf(success, "%s %s %s", "Purchase", inventBrandName, "Successfully !");
+    							write(connfd, success, strlen(success) + 1);
+    							confirm = 1;
+    							break;
+    						}
+    					}
+    				break;
+    				}
+    			}
+
+    			//send error message
+    			if(confirm == 0){
+    				sprintf(error, "%s %s %s", "Purchase", inventBrandName, "failed !");
+    				write(connfd, error, strlen(error));
+    			}
+
+    			//check inventory figures
+    			fseek(inventoryFile, 0, SEEK_SET);
+    			while(1){
+    				if((fgets(invent, 100, inventoryFile)) == NULL) break;
+
+    				//printf("invent %s", invent);
+    				inventMachineID = strtok(invent, ",");
+    				inventBrandName = strtok(NULL, ",");
+    				if(inventBrandName != NULL){
+    					inventStock = strtok(NULL, ",");
+    					stockNumber = atoi(inventStock);
+    					if(stockNumber <= 3){
+    						stockNumber = 10;
+    						sprintf(stock, "%d", stockNumber);
+    						fseek(inventoryFile, -4, SEEK_CUR);
+    						while((character = fgetc(inventoryFile)) != '\n'){
+    							if(isdigit((int)character)){
+    								fseek(inventoryFile, -1, SEEK_CUR);
+    								fprintf(inventoryFile, "%s\n", stock);
+    								//fseek(inventoryFile, 1, SEEK_CUR);
+    								printf("Delivering inventory !\n");
+
+    								//send message to vending machine to replenish the commodity
+    								//sprintf(clientMessage, "%s %s %d", machineID, brandName, stockNumber);
+    								//send(connfd, clientMessage, strlen(clientMessage), 0);
+    								//write(connfd, clientMessage, strlen(clientMessage));
+    								sleep(5);
+    								printf("Delivery completely!\n");
+    								break;
+    							}
+    						}
+    						msgFlag = 1;
+    						break;
+    					} 
+    				}
+    			}
+    			// printf("msgFlag %d\n", msgFlag);
+    			// if(msgFlag == 0){
+    			// 	send(connfd, "ok", 2, 0);
+    			// }
+
+    			fclose(inventoryFile);
+
+    			if(recvm == 0){
+    				printf("Client disconnected !\n");
+    			}
+    			else if (recvm ==-1){
+    				printf("recv failed !\n");
+    			}
+
+
+
+    			//access and write data to shared memory
+    			// if((shmidWrite = shmget(key, SHIMSIZE, IPC_CREAT | 0666 )) < 0){
+    			// 	printf("write shmget failed !\n");
+    			// }
+
+    			// if((head = shmat(shmidWrite, NULL, 0)) == (int *)-1){
+    			// 	printf("write shmat failed !\n");
+    			// }
+
+    			// shmMessage = (char *)malloc(100*sizeof(char));
+    			// strcpy(shmMessage, machineID);
+    			// strcat(shmMessage, " ");
+    			// strcat(shmMessage, brandName);
+    			// strcat(shmMessage, " ");
+    			// strcat(shmMessage, stock);
+    			// strcat(shmMessage, "\n");
+    			// memcpy(head, shmMessage, strlen(shmMessage));
+    		}	
+    		close(connfd);
+    	}
+    }
+
+    //check inventory figures from shared memory
+    // while(1){
+    // 	if((shmidRead = shmget(key, SHIMSIZE, IPC_CREAT | 0666 )) < 0){
+    // 		printf("read shmget failed !\n");
+    // 	}
+
+    // 	if((begin = shmat(shmidRead, NULL, 0)) == (int *)-1){
+    // 		printf("read shmat failed !\n");
+    // 	}
+
+
+    // }
+
+
+	return 0;
+}
---
+++ vendingmachine.c
@@ -0,0 +1,138 @@
+#include <stdio.h> 
+#include <stdlib.h>
+#include <string.h>   
+#include <sys/socket.h>   
+#include <arpa/inet.h> 
+#include <time.h>
+typedef struct Commodity_t{
+    int machineID[9];
+    char brandName[9][32];
+    int  stock[9];
+}Commodity;
+
+void initializeCommodity(Commodity *commodity);
+
+int main(){
+	Commodity commodity;
+	char* commoditySales;
+	int sockfd, commodity_bought, id, i;
+	struct sockaddr_in system;
+	pid_t pid;
+	time_t rawtime;
+    struct tm* currentTime;
+    char *msgMachineID, *msgStock, *msgYear, *msgMonth, *msgDate,*msgHour, *msgMinute, *msgSecond; 
+    char *replyMessage, *replyMachineID, *replyBrandName, *replyStock;
+	char *replyMessage2, *replyMachineID2, *replyBrandName2, *replyStock2;
+	initializeCommodity(&commodity);
+
+	//equipMain
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if(sockfd == -1){
+		printf("Create socket failed !\n");
+		return 0;
+	}
+
+	system.sin_addr.s_addr = inet_addr("127.0.0.1");
+	system.sin_family = AF_INET;
+	system.sin_port = htons(8888);
+
+	//connect to system
+	if(connect(sockfd, (struct sockaddr *)&system, sizeof(system))<0){
+		printf("Connect to system failed !\n");
+		return 0;
+	}
+
+	printf("May 1: nha D1\n0. Coca\n1. Milo\n2. Lavie\nMay 2: nha D2\n3. Pepsi\n4. Revive\n5. Nutri\nMay 3: nha D9\n6. Ovantine\n7. Sting\n8. Aquarius\n");
+
+	//create commodity Sales
+	pid = fork();
+		while(1){
+			if(pid == 0){
+				printf("Choose commodity: ");
+				scanf("%d", &commodity_bought);
+				commodity.stock[commodity_bought]--;
+				if(commodity.stock[commodity_bought] < 0){
+					printf("Sold out !\n");
+					break;
+				}
+				//get current time
+				time(&rawtime);
+		    	currentTime = localtime(&rawtime);
+				
+				msgMachineID = (char *)malloc(sizeof(char));
+				msgStock = (char *)malloc(2*(sizeof(char)));
+				msgYear = (char *)malloc(4*(sizeof(char)));
+				msgMonth = (char *)malloc(2*(sizeof(char)));
+				msgDate = (char *)malloc(2*(sizeof(char)));
+				msgHour = (char *)malloc(2*(sizeof(char)));
+				msgMinute = (char *)malloc(2*(sizeof(char)));
+				msgSecond = (char *)malloc(2*(sizeof(char)));
+				commoditySales = (char *)malloc(100*(sizeof(char)));
+				//create commoditySales information
+				sprintf(msgMachineID, "%d", commodity.machineID[commodity_bought]);
+				sprintf(msgStock, "%d", commodity.stock[commodity_bought]);
+				sprintf(msgYear, "%d", currentTime->tm_year + 1900);
+				sprintf(msgMonth, "%d", currentTime->tm_mon + 1);
+				sprintf(msgDate, "%d", currentTime->tm_mday);
+				sprintf(msgHour, "%d", currentTime->tm_hour);
+				sprintf(msgMinute, "%d", currentTime->tm_min);
+				sprintf(msgSecond, "%d", currentTime->tm_sec);
+				sprintf(commoditySales, "%s,%s,%s,%s,%s,%s,%s,%s,%s", 
+					msgMachineID, commodity.brandName[commodity_bought], msgStock, msgDate, msgMonth, msgYear, msgHour, msgMinute, msgSecond);
+				
+				if(send(sockfd, commoditySales, strlen(commoditySales)+1, 0) < 0){
+					printf("Can't send commodity Sales\n");
+				}
+
+				//receive reply
+				replyMessage = (char *)malloc(100 * sizeof(char));
+				if(recv(sockfd, replyMessage, 100, 0) < 0){
+					printf("recv failed !\n");
+					break;
+				}
+				printf("System reply: %s\n", replyMessage);
+
+				if(commodity.stock[commodity_bought] <= 3){
+					printf("Receiving commodity\n");
+					sleep(5);
+					commodity.stock[commodity_bought] = 10;
+					printf("Commodity has been replenished\n");
+				}
+				//determine which reply has been received
+				// replyMessage2 = (char *)malloc(100 * sizeof(char));
+				// if(recv(sockfd, replyMessage2, 100, 0) > 0){
+				// 	printf("replyMessage2 %s\n", replyMessage2);
+				// 	replyMachineID2 = strtok(replyMessage2, " ");
+				// 	replyBrandName2 = strtok(NULL, " ");
+				// 	if(replyBrandName2 != NULL){ 	//message to replenish commodity
+				// 		replyStock2 = strtok(NULL, " ");
+				// 		//find machine id and brandname
+				// 		for(i=0; i<9; i++){
+				// 			id = atoi(replyMachineID2);
+				// 			if(id == commodity.stock[i] && (strcmp(replyBrandName2, commodity.brandName[i]))==0){
+				// 				commodity.stock[i] = atoi(replyStock2);
+				// 				printf("Receiving commodity\n");
+				// 				sleep(5);
+				// 				printf("Commodity has been replenished\n");
+				// 			} 
+				// 		}
+				// 	}
+				// }
+			}
+		}
+		close(sockfd);
+	
+	return 0;
+}
+
+void initializeCommodity(Commodity *commodity){
+	int iMachineID[9] = {1, 1, 1, 2, 2, 2, 3, 3, 3};
+	char iBrandName[9][32] = {"Coca", "Milo", "Lavie", "Pepsi", "Revive", "Nutri", "Ovantine", "Sting", "Aquarius"};
+	int  iStock[9] = {10, 10, 10, 10, 10, 10, 10, 10, 10};
+	int i;
+	for (i = 0; i < 9; i ++){
+		commodity->machineID[i] = iMachineID[i];
+		strcpy(commodity->brandName[i], iBrandName[i]);
+		commodity->stock[i] = iStock[i];
+	}
+}
